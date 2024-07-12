584ed363d685777bab877c58c138a8283501564c
aniruddhas972 <139209135+aniruddhas972@users.noreply.github.com>
2024-07-12 15:16:10 +0530

    Add files via upload

---
+++ src/main/java/com/fusion/Digital/Customer/Onboarding/Service/CustomerDetailsService.java
@@ -0,0 +1,20 @@
+package com.fusion.Digital.Customer.Onboarding.Service;
+
+import com.fusion.Digital.Customer.Onboarding.Entity.CustomerDetailsEntity;
+import com.fusion.Digital.Customer.Onboarding.Entity.OtpDetailsEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface CustomerDetailsService {
+
+    public String saveCustomerDetails(CustomerDetailsEntity customerDetails);
+
+    public boolean checkUser(CustomerDetailsEntity customerDetails);
+
+    public void saveCust(CustomerDetailsEntity customerDetails);
+
+    public void updateCust(CustomerDetailsEntity customerDetails);
+
+}
